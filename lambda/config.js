b82f48c9d077caa17e6851dd77fe2307949dab61
leoniehandschmann <69795830+leoniehandschmann@users.noreply.github.com>
2025-11-01 12:05:10 +0100

    Update config.js

--- lambda/config.js
+++ lambda/config.js
@@ -1,5 +1,1 @@
-//config.js 
 
-const OPENAI_API_KEY = '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';
-
-module.export = { OPENAI_API_KEY };
